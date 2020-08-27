5b9bd903546ea964bdd6b9840df0b4ac8d244a04
Ian Nastajus <ian.nastajus@gmail.com>
2020-08-26 21:56:38 -0400

    added VGC and SASS, works
    except control-click in VSCODE with /index...sigh

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main/index'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main,
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+// SASS style sheet */
+// Palette color codes */
+// Palette URL: http://paletton.com/#uid=13j0u0kllllaFw0g0qFqFg0w0aF */
+
+$color-primary-0: #236467;	// Main Primary color */
+$color-primary-1: #67989A;
+$color-primary-2: #417E80;
+$color-primary-3: #0D4A4D;
+$color-primary-4: #003133;
+
+
+body {
+    background: $color-primary-0;
+    color: $color-primary-1;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Main/index.vue
@@ -0,0 +1,4 @@
+<template src="./Main.html"></template>
+<script src="./Main.js"></script>
+<style src="./Main.scss" scoped lang="scss"></style>
+
