e7119df6016b893407058cfc982b260f2e6a17d5
Ian Nastajus <ian.nastajus@gmail.com>
2020-08-26 22:56:43 -0400

    grid and plain CSS for tiling cards works

--- src/components/DeckClassifier.vue
+++ src/components/DeckClassifier.vue
@@ -3,9 +3,17 @@
   <section class="deck-classifier">
     <!-- <h1>deck-classifier Component</h1> -->
 
-    <div>One</div>
-    <div>Two</div>
-    <div>Three</div>
+    <div class="deck-classifier-column">One
+      <img src="./../assets/logo.png" width="100px"> 
+      <img src="./../assets/logo.png" width="100px"> 
+      <img src="./../assets/logo.png" width="100px"> 
+      <img src="./../assets/logo.png" width="100px"> 
+      <img src="./../assets/logo.png" width="100px"> 
+      <img src="./../assets/logo.png" width="100px"> 
+      <img src="./../assets/logo.png" width="100px"> 
+    </div>
+    <div class="deck-classifier-column">Two</div>
+    <div class="deck-classifier-column">Three</div>
 
   </section>
 
@@ -39,5 +47,15 @@
   .deck-classifier {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
+  }
+  .deck-classifier-column {
+    //display: grid;
+    // display: flex;
+    // flex-direction: column;
+
+    // flex-wrap: wrap;
+    // ////////// align-items: flex-start;
+    
+    text-align: left;
   }
 </style>
