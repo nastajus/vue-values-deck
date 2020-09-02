31a1eace5a630bbce3c6a76b227cc4b1929a5027
Ian Nastajus <ian.nastajus@gmail.com>
2020-09-02 16:50:35 -0400

    cleanup

--- src/main.js
+++ src/main.js
@@ -8,15 +8,13 @@ import { mapActions } from "vuex";
 Vue.config.productionTip = false
 
 new Vue({
-  // store: store, ECMA5
   store, //ECMA6
-  //computed: mapActions(["load"]), // computed vs methods...
+
   methods: mapActions(["load"]),    // question: is this even a valid location to do 'load'??
 
   created() {    
     this.load();
-  },
-  
+  },  
   
   render: h => h(App)
 }).$mount('#app')
